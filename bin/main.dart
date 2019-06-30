5a90c7014cfd8996197ff29f9f8f1a94b59e366f
Human-caching <brileydeveloper@gmail.com>
2019-06-30 02:40:56 -0400

    Initial code commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:bambam/bambam.dart' as bambam;
+
+main(List<String> arguments) {
+  print('Hello world: ${bambam.calculate()}!');
+}
---
+++ test/bambam_test.dart
@@ -0,0 +1,8 @@
+import 'package:bambam/bambam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
